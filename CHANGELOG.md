1dadc39310bd8db80c0f49b40563ad2d0ce1c000
Meissa Dia <meissadia@gmail.com>
2017-01-11 23:07:57 -0700

    v1.0.1.1 TestSuite: Replace hardcoded player info in testNbaPlayer with pattern matching

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 ### Change Log :: HoopScrape
 
+## Version 1.0.1.1
++ TestSuite: Replace hardcoded player info in testNbaPlayer with pattern matching
+
 ## Version 1.0.1
 + Bugfix: Double names (i.e. K. GarnettK. Garnett) in NbaBoxScore
 
--- test/testNbaPlayer.rb
+++ test/testNbaPlayer.rb
@@ -2,26 +2,18 @@ require_relative './test_helper'
 
 class TestNbaPlayer < Minitest::Test
   include NbaUrls
+  NAME = /^\w+\s?\w*$/
+  POSITION = /([A-Z]{2,}?)(\/[A-Z]{2,}?)?/
+  DIGIT = /^\d+$/
+  COLLEGE = /^\w+(?:\s?\w*)*$/
   def test_file_data
-    player = NbaPlayer.new('', 'test/data/playerBennett.html')
-    assert_equal 'Anthony Bennett', player.name,       'Player Name => Error'
-    assert_equal 'PF',              player.position,   'Player Position => Error'
-    assert_equal '6',               player.height_ft,  'Player H_FT => Error'
-    assert_equal '8',               player.height_in,  'Player H_IN => Error'
-    assert_equal '245',             player.weight,     'Player Weight => Error'
-    assert_equal 'UNLV',            player.college,    'Player College => Error'
-    assert_equal '23',              player.age,        'Player Age => Error'
+    a_bennett = NbaPlayer.new('', 'test/data/playerBennett.html')
+    validate_player(a_bennett)
   end
 
   def test_live_data
-    player = HoopScrape.player(1966)
-    assert_equal 'LeBron James', player.name,     'Player Name => Error'
-    assert_equal 'None',         player.college,  'Player College => Error'
-    assert_equal 'SF',           player.position, 'Player Position => Error'
-    assert_equal '31',           player.age,      'Player Age => Error'
-    assert_equal '6',            player.h_ft,     'Player H_FT => Error'
-    assert_equal '8',            player.h_in,     'Player H_IN => Error'
-    assert_equal '250',          player.weight,   'Player Weight => Error'
+    l_james = HoopScrape.player(1966)
+    validate_player(l_james)
   end
 
   def test_live_non_nba
@@ -33,5 +25,15 @@ class TestNbaPlayer < Minitest::Test
     HoopScrape.player(2233514)
     HoopScrape.player(2995725)
     HoopScrape.player(3945381)
+  end
+
+  def validate_player(player)
+    assert NAME.match(player.name),          'Player Name => Error'
+    assert COLLEGE.match(player.college),    'Player College => Error'
+    assert POSITION.match(player.position),  'Player Position => Error'
+    assert DIGIT.match(player.age),          'Player Age => Error'
+    assert DIGIT.match(player.height_ft),    'Player H_FT => Error'
+    assert DIGIT.match(player.height_in),    'Player H_IN => Error'
+    assert DIGIT.match(player.weight),       'Player Weight => Error'
   end
 end
